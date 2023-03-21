db82bb9abe439551c7ba22d67367199ee1884bc6
hyeonwater <hs97kim@gmail.com>
2023-03-21 22:09:37 +0900

    check user win case && made win,draw dialog && reset button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:hyeonsookim_tic_tac_toe/pages/gamePage.dart';
 
 void main() {
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       home: Scaffold(
         body: GamePage(),
       ),
--- lib/pages/gamePage.dart
+++ lib/pages/gamePage.dart
@@ -1,6 +1,8 @@
 import 'dart:math';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:hyeonsookim_tic_tac_toe/common.dart';
 
 class GamePage extends StatefulWidget {
@@ -16,12 +18,11 @@ class _GamePageState extends State<GamePage> {
   late List<String> emptyCard;
   late String currentPlayer = randomPlayer();
   bool changeTurn = true;
-
+  int drawNum = 0;
 
   @override
   void initState() {
     initializeGame();
-    print(currentPlayer);
     super.initState();
   }
 
@@ -33,6 +34,7 @@ class _GamePageState extends State<GamePage> {
   }
   void initializeGame() {
     emptyCard = ["", "", "", "", "", "", "", "", ""];
+    drawNum = 0;
   }
   @override
   Widget build(BuildContext context) {
@@ -53,6 +55,15 @@ class _GamePageState extends State<GamePage> {
                   return _card(index);
                 }),
           ),
+          CupertinoButton(
+            child: Text('다시 시작하기'),
+            onPressed: () {
+              setState(() {
+                initializeGame();
+                drawNum = 0;
+              });
+            },
+          )
         ],
       ),
     );
@@ -65,17 +76,21 @@ class _GamePageState extends State<GamePage> {
           setState(() {
             if(changeTurn){
                 emptyCard[index] = currentPlayer == 'playerOne' ? 'O' : 'X';
+                drawNum += 1;
             }else{
               emptyCard[index] = currentPlayer == 'playerOne' ? 'X' : 'O';
+              drawNum += 1;
             }
           });
+          print(drawNum);
           changeTurn = !changeTurn;
           _checkWin();
         }
       },
       child: Container(
         color: emptyCard[index] == 'O' ? Colors.green :Colors.grey,
-        child: Center(child: Text(emptyCard[index])),
+        child: Center(
+            child: Text(emptyCard[index])),
       ),
     );
   }
@@ -87,39 +102,73 @@ class _GamePageState extends State<GamePage> {
     /// 6 7 8
     /// 이런 모양의 그리드뷰가 생성
 
-    /// 0번 데이터가 비어있지 않거나 0번 데이터가 1번이랑 똑같거나 2번이랑 똑같은 경우
+    /// 0번 데이터가 비어있지 않고 0번 데이터가 1번이랑 똑같고 2번이랑 똑같은 경우
     if(emptyCard[0].isNotEmpty && emptyCard[0] == emptyCard[1] && emptyCard[0] == emptyCard[2] ){
       _winDialog(emptyCard[0]);
     }
-    /// 3번 데이터가 비어있지 않거나 3번 데이터가 4번이랑 똑같거나 5번이랑 똑같은 경우
+    /// 3번 데이터가 비어있지 않고 3번 데이터가 4번이랑 똑같고 5번이랑 똑같은 경우
     if(emptyCard[3].isNotEmpty && emptyCard[3] == emptyCard[4] && emptyCard[3] == emptyCard[5]){
       _winDialog(emptyCard[3]);
     }
-    /// 6번 데이터가 비어있지 않거나 6번 데이터가 7번이랑 똑같거나 8번이랑 똑같은 경우
+    /// 6번 데이터가 비어있지 않고 6번 데이터가 7번이랑 똑같고 8번이랑 똑같은 경우
     if(emptyCard[6].isNotEmpty && emptyCard[6] == emptyCard[7] && emptyCard[6] == emptyCard[8]){
       _winDialog(emptyCard[6]);
     }
-    /// 1번 데이터가 비어있지 않거나 1번 데이터가 4번이랑 똑같거나 7번이랑 똑같은 경우
+    /// 1번 데이터가 비어있지 않고 1번 데이터가 4번이랑 똑같고 7번이랑 똑같은 경우
     if(emptyCard[1].isNotEmpty && emptyCard[1] == emptyCard[4] && emptyCard[1] == emptyCard[7]){
       _winDialog(emptyCard[1]);
     }
-    /// 2번 데이터가 비어있지 않거나 2번 데이터가 5번이랑 똑같거나 8번이랑 똑같은 경우
+    /// 2번 데이터가 비어있지 않고 2번 데이터가 5번이랑 똑같고 8번이랑 똑같은 경우
     if(emptyCard[2].isNotEmpty && emptyCard[2] == emptyCard[5] && emptyCard[2] == emptyCard[8]){
       _winDialog(emptyCard[2]);
     }
-    /// 0번 데이터가 비어있지 않거나 0번 데이터가 4번이랑 똑같거나 8번이랑 똑같은 경우
+    /// 0번 데이터가 비어있지 않고 0번 데이터가 4번이랑 똑같고 8번이랑 똑같은 경우
     if(emptyCard[0].isNotEmpty && emptyCard[0] == emptyCard[4] && emptyCard[0] == emptyCard[8]){
       _winDialog(emptyCard[0]);
     }
-    /// 2번 데이터가 비어있지 않거나 2번 데이터가 4번이랑 똑같거나 6번이랑 똑같은 경우
+    /// 2번 데이터가 비어있지 않고 2번 데이터가 4번이랑 똑같고 6번이랑 똑같은 경우
     if(emptyCard[2].isNotEmpty && emptyCard[2] == emptyCard[4] && emptyCard[2] == emptyCard[6]){
       _winDialog(emptyCard[2]);
     }
+    else if(drawNum == 9){
+      _drawDialog();
+    }
   }
+
+  void _drawDialog(){
+    showDialog(barrierDismissible: false,
+        context: context, builder: (context){
+      return CupertinoAlertDialog(
+        title: Text('비김'),
+        content: CupertinoButton(
+          child: Text('확인'),
+          onPressed: () {
+            Get.back();
+            setState(() {
+              initializeGame();
+            });
+          },
+        ),
+      );
+    });
+  }
+
   void _winDialog(String winner){
-    showDialog(context: context, builder: (context){
-      return AlertDialog(
-        title: Text('${winner != 'O' ? '플레이어 1' : '플레이어 2' }이(가) 이겼습니다.'),
+    showDialog(
+        barrierDismissible: false,
+        context: context, builder: (context){
+      return CupertinoAlertDialog(
+        title: Text('${winner != 'X' ? '플레이어 1' : '플레이어 2' }이(가) 이겼습니다.'),
+        content: CupertinoButton(
+          child: Text('확인'),
+          onPressed: () {
+            Get.back();
+            setState(() {
+              initializeGame();
+              }
+            );
+          },
+        ),
       );
     });
 }
